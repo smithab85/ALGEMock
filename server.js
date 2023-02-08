6f6d7553dedd1df8f3d55ec1e79f6ee921929ad5
Kyle Senebouttarath <kylesenebouttarath@gmail.com>
2023-02-08 13:32:24 -0600

    Docs

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const PORT = 3000
 const THRESHOLD = 95    //non-inclusive
 const WEATHER_API_KEY = "816e97fdecd8428ab65201454230102"
-
-const uri = "mongodb+srv://root:root@cluster0.hpqj1xl.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const data = client.db("weather").collection("data");
+const URI = "mongodb+srv://root:root@cluster0.hpqj1xl.mongodb.net/?retryWrites=true&w=majority";
 
 //---------------- GLOBALS ----------------//
 
 const app = express();
 const webpageDir = `${__dirname}`;
+
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const data = client.db("weather").collection("data");
 
 let belowThresholdRNGCount = 0;
 let rngCount = 0;
@@ -31,15 +31,24 @@ app.use(bodyParser.json());
 
 //---------------- FUNCTIONS ----------------//
 
-/*
-Example locations:
-    Lake Geneva: 53147
+/**
+ * Example locations:
+ *     Lake Geneva: 53147
+ *
+ * @param loc A location to get data from. Can be a city name, zip code, or latitude/longitude
+ * @return {`https://api.weatherapi.com/v1/current.json?key=816e97fdecd8428ab65201454230102&q=${string}&aqi=no`}
  */
 const getWeatherAPI_URL = (loc) => {
     return `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${loc}&aqi=no`;
 }
 
-const getValueFromUrl = (url, cb) => {
+/**
+ * Using HTTPS, get data from a provided URL and perform an action
+ * on said data with a callback function
+ * @param url URL to get data from
+ * @param cb Callback function. First parameter is an error, second parameter is returned JSON data
+ */
+const getJSONDataFromUrl = (url, cb) => {
     https.get(url, (resp) => {
         let data = ''
         let value = ''
@@ -53,9 +62,20 @@ const getValueFromUrl = (url, cb) => {
     });
 }
 
+/**
+ * Retrieves weather data from the WeatherAPI website and performs an action
+ * on said data with a callback function.
+ * @param loc Location. Can be a ZIP, city name, or lat/long pair (in degrees)
+ * @param cb Callback function to perform data on. First parameter is an object like this
+ *  {
+ *      err: an error object from the HTTPS request. if this exists, other fields may be null
+ *      temp_c: <temperature number in celsius>
+ *      wind_kph: <air speed number>
+ *  }
+ */
 const getWeatherData = (loc, cb) => {
     const url = getWeatherAPI_URL(loc);
-    getValueFromUrl(url, (err, jsonData) => {
+    getJSONDataFromUrl(url, (err, jsonData) => {
         if (err) return console.error(err);
         let windKPH = undefined, tempCelsius = undefined, error = undefined;
         if (jsonData && jsonData.current) {
@@ -106,7 +126,6 @@ app.post("/location", async (request, response) => {
 const listener = app.listen(PORT, () => {
     console.log("Your app is listening on port " + PORT);
 });
-
 
 setTimeout(async () => {
     getWeatherData(53147, async (retObj) => {
